aaad1c14463101577f23935cf6d148c38661e9fd
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-12 23:14:57 +0200

    logs the request and response

--- src/main/java/com/example/springbootlogging/WebController.java
+++ src/main/java/com/example/springbootlogging/WebController.java
@@ -1,5 +1,7 @@
 package com.example.springbootlogging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
 
+  Logger log = LoggerFactory.getLogger(WebController.class);
+
   @GetMapping("/{name}")
   public String greeting (@PathVariable(name = "name") String name){
-    return "Helloss " + name + " :)";
+
+    // um strings Konkatenation zu vermeiden, sieht der Massage bei sl4j so.. ("Request {}", name)
+    log.debug("Request {}", name);
+    String response = "Helloss " + name + " :)";
+    log.debug("Response {}", response);
+
+    return response;
   }
 
 }
